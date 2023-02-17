eec41eb0eb07bafcc1460bda0d0d0d0911e67368
CarwynKurt <carwynkurtarimado@gmail.com>
2023-02-17 13:19:41 +0800

    Values for chart

--- Visual Sorting Algorithm.py
+++ Visual Sorting Algorithm.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Values for chart
+amount = 15
+
+lst = np.random.randint(0, 100, 15)
+x = np.arange(0, amount, 1)
+
 # Bubble sort
 # Plotting
 # Bubble sort plot
