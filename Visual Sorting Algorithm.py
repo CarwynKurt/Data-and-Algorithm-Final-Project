230bf16897ded78ca6dce9ff812acecbbe5261c5
CarwynKurt <carwynkurtarimado@gmail.com>
2023-02-17 23:31:36 +0800

    Selection Sort

--- Visual Sorting Algorithm.py
+++ Visual Sorting Algorithm.py
@@ -44,6 +44,22 @@ def InsertionSort(amount):
     plt.show()
 
 # Selection Sort
+def SelectionSort(amount):
+    lst = np.random.randint(0, 100, amount)
+    x = np.arange(0, amount, 1)
+    n = len(lst)
+
+    for i in range(0, n - 1):
+        smallest = i
+        for j in range(i + 1, n):
+            plt.bar(x, lst)
+            plt.pause(0.001)
+            plt.clf()
+            if lst[j] < lst[smallest]:
+                smallest = j
+        lst[i], lst[smallest] = lst[smallest], lst[i]
+    plt.show()
+
 # Merge Sort
 # Input Values
 # Menu
