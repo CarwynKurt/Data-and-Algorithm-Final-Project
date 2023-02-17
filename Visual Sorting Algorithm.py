7c17dc339b41cc9f921799d4347483641b847b24
CarwynKurt <carwynkurtarimado@gmail.com>
2023-02-17 22:53:05 +0800

    Bubble Sort

--- Visual Sorting Algorithm.py
+++ Visual Sorting Algorithm.py
@@ -7,6 +7,21 @@ import numpy as np
 import random
 
 # Bubble Sort
+def BubbleSort(amount):
+    lst = np.random.randint(0, 100, amount)
+    x = np.arange(0, amount, 1)
+    n = len(lst)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            plt.bar(x, lst)
+            plt.pause(0.001)
+            plt.clf()
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+    plt.show()
+
 # Insertion Sort
 # Selection Sort
 # Merge Sort
